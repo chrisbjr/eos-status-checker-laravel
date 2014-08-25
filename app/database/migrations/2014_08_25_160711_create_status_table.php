a223b3a3101819925caf2c0d06f853807b980d20
Chris Bautista <chrisbjr@gmail.com>
2014-08-26 00:11:52 +0800

    Added migration of "status" table

---
+++ app/database/migrations/2014_08_25_160711_create_status_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('status', function ($table) {
+            $table->increments('id');
+            $table->boolean('north_america');
+            $table->boolean('europe');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+
+}
